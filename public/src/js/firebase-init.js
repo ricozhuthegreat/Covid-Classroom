930437971ec6842535922e189a0f91deb9dd44c8
ricozhuthegreat <ricozhuthegreat@hotmail.com>
2020-03-28 02:59:11 -0400

    Added Firebase Initiation in JS
     - Created a new file: firebase-init.js that will initialize the firebase app upon buffering after the deferred core Firebase JS SDK CDN

---
+++ public/src/js/firebase-init.js
@@ -0,0 +1,14 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxHGe9coGIJVbAIQxI-lftFOBhRQpKv7c",
+  authDomain: "covid-classroom.firebaseapp.com",
+  databaseURL: "https://covid-classroom.firebaseio.com",
+  projectId: "covid-classroom",
+  storageBucket: "covid-classroom.appspot.com",
+  messagingSenderId: "612240306575",
+  appId: "1:612240306575:web:22f0534b8ffd4cc6bd5bfa",
+  measurementId: "G-P7XYZFRE98"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
